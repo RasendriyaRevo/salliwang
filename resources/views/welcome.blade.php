6892685fb3c5fb795e6f88a2cefee7e613ef0dd3
RasendriyaRevo <revodaniswara@gmail.com>
2019-06-09 12:14:03 +0800

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+@extends('header')
+@section('title','SALLI WANG PRODUCTION')
+@section('content')
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color:transparent">
+    <a class="navbar-brand" href="#"><img style="max-width:25%;max-height:25%" src="img/logo.gif"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav my-2 my-lg-0">
+            <a class="nav-item nav-link" href="#">Home</a>
+            <a class="nav-item nav-link" href="#">Portofolio</a>
+            <a class="nav-item nav-link" href="#">About</a>
+            <a class="nav-item nav-link" href="#">Contact</a>
+        </div>
+    </div>
+</nav>
+@endsection
